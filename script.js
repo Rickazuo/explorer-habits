8aed701861ebc9385805a02219d20e324bfdba1c
Rickazuo <rick3209@gmail.com>
2023-01-21 19:27:01 -0300

    add-localstorage

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
-  water: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
-  exercise: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
-  sleep: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
-  dog: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
-  food: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+  
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+
+  const dayExists =nlwSetup.dayExists(today)
+
+  if(dayExists) {
+    alert("Data já inclusa")
+    return
+  }
+
+  nlwSetup.addDay(today)
+
 }
 
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
